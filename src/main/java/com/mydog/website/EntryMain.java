1370aee06134bf00065b8353bab135abe8d914a5
hanxueming@testin.cn <hanxueming@testin.cn>
2018-08-10 18:35:10 +0800

    controller

--- src/main/java/com/mydog/website/EntryMain.java
+++ src/main/java/com/mydog/website/EntryMain.java
@@ -1,10 +1,14 @@
 package com.mydog.website;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@MapperScan("com.mydog.dao")
 @SpringBootApplication
+@EnableTransactionManagement
 public class EntryMain extends SpringBootServletInitializer{
     public static void main(String[] args){
         SpringApplication.run(EntryMain.class);
---
+++ src/main/java/com/mydog/website/controller/BaseController.java
@@ -0,0 +1,7 @@
+package com.mydog.website.controller;
+
+public class BaseController {
+
+    
+
+}
